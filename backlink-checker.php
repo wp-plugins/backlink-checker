0fff8ad48ddb3b6d693a11b5f4d275b12c477da8
sunnyverma1984 <sunnyverma1984@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-01 07:28:14 +0000

    first release
    git-svn-id: https://plugins.svn.wordpress.org/backlink-checker/tags/1.0@1210799 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ backlink-checker.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Backlink Checker
+Plugin URI:  https://wordpress.org/plugins/backlink-checker/
+Description: A WordPress plugin that let you check backlinks using free MOZ API
+Version:     1.0
+Author:      Sunny Verma
+Author URI:  http://99webtools.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /lang
+Text Domain: backlink
+*/
+add_action( 'plugins_loaded', 'backlink_load_textdomain' );
+function backlink_load_textdomain() {
+  load_plugin_textdomain( 'backlink', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' ); 
+}
+
+add_action('admin_menu', 'backlink_menu');
+function backlink_menu() {
+	add_options_page(__('Backlink Checker Settings','backlink'), __('Backlink Checker','backlink'), 'manage_options', 'backlink-checker.php', 'backlink_page_function');
+	add_menu_page( __('Backlink Report','backlink'), __('Backlink Report','backlink'), 'manage_options', 'backlink-report.php', 'backlink_report', 'dashicons-admin-links', 76 );
+	add_action( 'admin_init', 'register_backlink_setting' );
+}
+function register_backlink_setting() {
+	register_setting( 'backlink_options', 'mozmember' ); 
+	register_setting( 'backlink_options', 'mozsecret' ); 
+} 
+function backlink_page_function(){
+?>
+<div class="wrap">
+		<h2><?php _e('Backlink Checker Settings','backlink'); ?></h2>
+        <form method="post" action="options.php">
+<?php
+ settings_fields('backlink_options');
+ do_settings_sections( 'backlink_options' );
+?>
+<div class='wrap'>
+ <table class="form-table">
+        <tr valign="top">
+        <th scope="row"><?php _e('Moz Access ID','backlink'); ?></th>
+        <td><input type="text" name="mozmember" value="<?php echo esc_attr( get_option('mozmember') ); ?>" /></td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row"><?php _e('Moz Secret Key','backlink'); ?></th>
+        <td><input type="text" name="mozsecret" value="<?php echo esc_attr( get_option('mozsecret') ); ?>" /></td>
+        </tr>
+    </table>
+    <?php submit_button();?>
+</form>
+<?php printf(__('To get MOZ API credentials visit %s','backlink'),'<a href="https://moz.com/products/api" target="_blank">https://moz.com/products/api</a>'); ?></div>
+</div>
+<?php
+}
+
+function get_moz_backlinks()
+{
+	$accessID = get_option('mozmember');
+	$secretKey = get_option('mozsecret');
+	// Set your expires for five minutes into the future.
+	$expires = time() + 300;
+	// A new linefeed is necessary between your AccessID and Expires.
+	$stringToSign = $accessID."\n".$expires;
+	// Get the "raw" or binary output of the hmac hash.
+	$binarySignature = hash_hmac('sha1', $stringToSign, $secretKey, true);
+	// We need to base64-encode it and then url-encode that.
+	$urlSafeSignature = urlencode(base64_encode($binarySignature));
+	// This is the URL that we want link metrics for.
+	$objectURL = get_site_url();
+	// Now put your entire request together.
+	// This example uses the Mozscape URL Metrics API.
+	$requestUrl = "http://lsapi.seomoz.com/linkscape/links/".urlencode($objectURL)."?Scope=page_to_subdomain&SourceCols=4&LinkCols=10&TargetCols=0&Limit=100&Filter=external&AccessID=".$accessID."&Expires=".$expires."&Signature=".$urlSafeSignature;
+	$content = wp_remote_get($requestUrl,array('timeout'=>60));
+	return json_decode($content['body'],true);
+}
+function backlink_report()
+{
+	$err=0;
+	if(false===($data=get_transient('backlink_report')))
+	{
+		if(get_option('mozsecret') && get_option('mozmember'))
+		{
+		$data=get_moz_backlinks();
+		set_transient('backlink_report',$data,WEEK_IN_SECONDS);
+		}
+		else 
+		$err=1;
+	}
+	if($err)
+	{
+		echo '<h2>'.__('Backlink Report','backlink').'</h2>';
+		echo '<div class="error">'.sprintf(__('Please goto <a href="%s">Settings Page</a>. ','backlink'),admin_url( 'options-general.php?page=backlink-checker.php' ));
+		_e('And add MOZ API Credentials.','backlink');
+		echo '<br>';
+		printf(__('To get MOZ API credentials visit %s','backlink'),'<a href="https://moz.com/products/api" target="_blank">https://moz.com/products/api</a>');
+		 echo '</div>';
+	}
+	else
+	{
+	?>
+	<h2><?php _e('Backlink Report','backlink'); ?></h2>
+	<table class="widefat striped" cellspacing="0">
+<thead><tr><th>#</th><th><?php _e('Backlink','backlink'); ?></th><th><?php _e('Anchor Text','backlink'); ?></th><th><?php _e('Type','backlink'); ?></th></tr></thead>
+<tbody id="the-list">
+	<?php
+	$i=0;
+	if(count($data))
+	{
+	foreach($data as $r)
+	{
+		$i++;
+	?>
+	<tr><td><?php echo $i; ?></td><td><a href="http://<?php echo $r['uu']; ?>" target="_blank"><?php echo $r['uu']; ?></a></td><td><?php echo $r['lnt']; ?></td><td><?php echo ($r['lf']&1)?"Nofollow":"Dofollow"; ?></td></tr>
+	<?php
+}
+}
+else
+echo "<tr><td colspan=\"4\">".__('No Backlink Found','backlink')."</td></tr>"
+?>
+</tbody>
+</table>
+<?php
+}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Backlink Checker ===
+
+Contributors: sunnyverma1984
+Donate link: http://99webtools.com/
+Tags: backlink, backlink checker, inbound links, link popularity, search engine optimization, seo, seo stats, statistics
+Requires at least: 3.5
+Tested up to: 4.2.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Get the list of backlinks pointing to your blog
+
+== Description ==
+
+Backlinks are crucial to SEO. Google gives more importance to website that have higher number of quality backlinks.
+A website with good amount of backlinks will get higher position in SERP.
+
+**Backlink Checker Plugin** helps blogger to find list of webpages pointing their blog using anchor tag. 
+This plugin also shows anchor text and type of backlink (doFollow or noFollow).
+
+This plugin uses free MOZ API to generate backlink report.
+To use this plugin you have to get free [moz account](https://moz.com/community/join) with [API access](https://moz.com/products/api).
+
+For Demo See Our [Backlink Checker](http://backlinkchecker.us) tool.
+
+== Installation ==
+
+= Using the Plugin Manager =
+
+1. Click Plugins
+2. Click Add New
+3. Search for `backlink-checker`
+4. Click Install
+5. Click Install Now
+6. Click Activate Plugin
+
+= Manually =
+
+1. Upload `backlink-checker` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+= After activate =
+
+* Get [MOZ API Credentials](https://moz.com/products/api)
+* Goto `Backlink Checker Settings` page under option section
+* Add your API Credentials `Moz Access ID` and `Moz Secret Key`
+* Done
+
+== Screenshots ==
+
+1. Settings Page screenshot
+2. Backlink Report Page screenshot
+
+== Frequently Asked Questions ==
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
